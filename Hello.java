4f548918f452fb8e785eca734c002c1a734a06dd
David Crowley <53307335+physcrowley@users.noreply.github.com>
2023-10-16 11:58:14 -0400

    restore Hello.java to basic state

--- Hello.java
+++ Hello.java
@@ -1,8 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Bonjour");
-        System.out.println("Bonjour encore");
-        System.out.println("Bonjour à partir du Codespace web");
-        System.out.println("Bonjour à partir du Codespace distant via mon VS Code local");
     }
 }
